a6113688dbd8ac2fc9e9c5f070bb8cd5f715bd52
myoung900516-diamond <sophie0516@gmail.com>
2026-07-13 17:49:10 +0900

    국가 복합검색

--- reactjs/home/src/templates/Body.jsx
+++ reactjs/home/src/templates/Body.jsx
@@ -1,15 +1,20 @@
 import { Route, Routes } from "react-router-dom";
+
 import Home from "@components/Home";
+
 import BookList from "@components/book/BookList";
 import BookAdd from "@components/book/BookAdd";
 import BookDetail from "@components/book/BookDetail";
 import BookEdit from "@components/book/BookEdit";
 import BookSpa from "@components/book/BookSpa";
+
 import CountryList from "@components/country/CountryList";
 import CountryAdd from "@components/country/CountryAdd";
 import CountryDetail from "@components/country/CountryDetail";
 import CountryEdit from "@components/country/CountryEdit";
-import CountrySearch from "../components/country/CountrySearch";
+import CountrySearch from "@components/country/CountrySearch";
+import CountryComplexSearch from "@components/country/CountryComplexSearch";
+
 import LectureAdd from "@components/lecture/LectureAdd";
 import LectureList from "@components/lecture/LectureList";
 import LectureDetail from "@components/lecture/LectureDetail";
@@ -22,21 +27,26 @@ export default function Body(){
     return(
         <Routes>
             <Route path="/" element={<Home/>}></Route>
+
             <Route path="/country/list" element={<CountryList/>}></Route>
             <Route path="/country/add" element={<CountryAdd/>}></Route>
             {/* 제일 마지막에 적혀있는 값을 countryNo라는 이름으로 관리하겠다. */}
             <Route path="/country/detail/:countryNo" element={<CountryDetail/>}></Route>
             <Route path="/country/edit/:countryNo" element={<CountryEdit/>}></Route>
             <Route path="/country/search" element={<CountrySearch/>}></Route>
+            <Route path="/country/complex" element={<CountryComplexSearch/>}></Route>
+
             <Route path="/lecture/list" element={<LectureList/>}></Route>
             <Route path="/lecture/add" element={<LectureAdd/>}></Route>
             <Route path="/lecture/detail/:lectureNo" element={<LectureDetail/>}></Route>
             <Route path="/lecture/edit/:lectureNo" element={<LectureEdit/>}></Route>
+
             <Route path="/book/list" element={<BookList/>}></Route>
             <Route path="/book/spa" element={<BookSpa/>}></Route>
             <Route path="/book/add" element={<BookAdd/>}></Route>
             <Route path="/book/detail/:bookId" element={<BookDetail/>}></Route>
             <Route path="/book/edit/:bookId" element={<BookEdit/>}></Route>
+            
             <Route path="*" element={<NotFound/>}></Route>
         </Routes>
     )
